a674fc4b05cc50727f3fa48ea1dffe18b59fa8bc
Junru Ren <thomasren212@gmail.com>
2017-02-16 21:35:12 -0800

    Fixed invalid LED Pin const name, and added comment on analogWrite() supported pins

--- BlinkingLED/BlinkingLED.ino
+++ BlinkingLED/BlinkingLED.ino
@@ -5,26 +5,27 @@
  * Last Update: Feb 12, 2017
  * Remark: Be sure to have a resister connected in series with the LED to 
  *         protect the circuit.
+ *         analogWrite() only available for Pin 3, 5, 6, 8, 10, and 11 on most Arduino boards
  */
 
 /* Constant Data Field */
-const int PINOUT_LED = 9;       // Initialize the LED pinout to be Pin 9
+const int PIN_LED = 9;          // Initialize the LED pinout to be Pin 9
 const int ANALOG_DELAY = 5;     // Delay between each increment/decrement step of the LED
 const int DELAY_GAP = 500;      // The delay gap (in ms)
 const int MAX_DUTY_CYCLE = 255; // Maximum output value allowed for analogWrite()
 
 void setup() {
   // Initialize the output pin
-  pinMode(PINOUT_LED, OUTPUT);
+  pinMode(PIN_LED, OUTPUT);
 }
 
 void loop() {
   /* Digital Blink */
   // Turn on the LED
-  digitalWrite(PINOUT_LED, HIGH);
+  digitalWrite(PIN_LED, HIGH);
   delay(DELAY_GAP);
   // Turn off the LED
-  digitalWrite(PINOUT_LED, LOW);
+  digitalWrite(PIN_LED, LOW);
   delay(DELAY_GAP);
 
   /* Analog Blink */
